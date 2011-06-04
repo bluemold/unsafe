6974355275b9ad196ad89cf2c014befd33750a76
Neil Essy <neilessy@gmail.com>
2011-06-04 09:26:49 -0700

    Converted to a maven project/structure

---
+++ src/test/java/org/bluemold/unsafe/UnsafeTest.java
@@ -0,0 +1,18 @@
+package org.bluemold.unsafe;
+
+import org.junit.Test;
+
+/**
+ * UnsafeTest<br/>
+ * Author: Neil Essy<br/>
+ * Created: 6/4/11<br/>
+ * <p/>
+ * [Description]
+ */
+public class UnsafeTest
+{
+	@Test
+	public void noTest() {
+		
+	}
+}
